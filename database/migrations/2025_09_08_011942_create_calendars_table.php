678cc24025e801d2194e82cac1793a41665492b6
yasaralrabea <yasaralrabei255@gmail.com>
2025-09-24 10:26:22 +0300

    Update 2025_09_08_011942_create_calendars_table.php

--- database/migrations/2025_09_08_011942_create_calendars_table.php
+++ database/migrations/2025_09_08_011942_create_calendars_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('calendarders', function (Blueprint $table) {
+        Schema::create('calendars', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('goal');
